4d16660253a61b7ca6322faebc2e0932cd462798
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:28:47 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,11 +1,3 @@
 #ifndef TYPEWISE_ALERT_H
 #define TYPEWISE_ALERT_H
-
-typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
-} AlertTarget;
-
-
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 #endif
