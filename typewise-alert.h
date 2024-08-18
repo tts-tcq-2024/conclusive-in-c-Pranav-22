8a2c2a0ce15fdb1cabe225384779e72dd78ed649
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:50:08 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,21 +1,6 @@
 #ifndef TYPEWISE_ALERT_H
 #define TYPEWISE_ALERT_H
 
-
-
-typedef enum
-{
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
-
-typedef struct {
-  CoolingType coolingType;
-  char brand[48];
-} BatteryCharacter;
-
-
 typedef enum {
   TO_CONTROLLER,
   TO_EMAIL
@@ -23,8 +8,5 @@ typedef enum {
 
 
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
 
 #endif
