e09a17e7a5683619df40dfcbe4fa409da0639158
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:49:03 +0530

    Update common_util.h

--- common_util.h
+++ common_util.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+typedef enum
+{
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
 void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
 
 #endif
