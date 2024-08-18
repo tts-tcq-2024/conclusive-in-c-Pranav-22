45cede307d7df5453361b757a6adf200d417c1f4
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 16:10:05 +0530

    Update message_logger.cpp

--- message_logger.cpp
+++ message_logger.cpp
@@ -8,11 +8,9 @@ int printcount;
 
 void print(const char *format, ...)
 {
-    static int count = 0;
     va_list args;
     va_start(args, format);
     vsprintf(testbuffer[count], format, args);
     va_end(args);
     printcount++;
-    count++;
 }
