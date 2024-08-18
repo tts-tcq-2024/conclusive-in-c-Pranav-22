7b932a98fc765625b8b8241510fe09d7a6b0d929
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 11:51:25 +0530

    Create send_to_email.c

---
+++ send_to_email.c
@@ -0,0 +1,29 @@
+#include "message_logger.h"
+#include "send_to_email.h"
+
+void sendHighTempAlert(const char* recepient)
+{
+    print("To: %s\n", recepient);
+    print("Hi, the temperature is too high\n");
+}
+
+void sendLowTempAlert(const char* recepient)
+{
+    print("To: %s\n", recepient);
+    print("Hi, the temperature is too low\n");
+}
+
+void onNormalTemp(const char* recepient)
+{
+   return;
+}
+
+void sendToEmail(BreachType breachType)
+{
+  const char* recepient = "a.b@c.com";
+
+  void (*messageType[])(const char* recepient) = {onNormalTemp,sendLowTempAlert,sendHighTempAlert};
+
+  messageType[breachType](recepient);
+
+}
