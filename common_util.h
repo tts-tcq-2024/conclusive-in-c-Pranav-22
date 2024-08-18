784be1dab1cb2d53ddf2259cd7ccb961520c80b2
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:07:27 +0530

    Update common_util.h

--- common_util.h
+++ common_util.h
@@ -13,6 +13,14 @@ typedef enum
   TOO_HIGH
 } BreachType;
 
+typedef enum
+{
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
+
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 
