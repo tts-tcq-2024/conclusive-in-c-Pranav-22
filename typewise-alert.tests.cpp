a516684c5f1830efc23bbc8fdef7c9694aa7bbae
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 15:35:57 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -11,3 +11,12 @@ TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
     checkAndAlert(TO_CONTROLLER, batteryChar, temperatureInC);
     EXPECT_STREQ("feed : 2",testbuffer[0]);
 }
+
+
+TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+    BatteryCharacter batteryChar;
+    batteryChar.coolingType = HI_ACTIVE_COOLING;
+    double temperatureInC = 50.0;
+    checkAndAlert(TO_EMAIL, batteryChar, temperatureInC);
+    EXPECT_STREQ("Hi, the temperature is too high",testbuffer[2]);
+}
