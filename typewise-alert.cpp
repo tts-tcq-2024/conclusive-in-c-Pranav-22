b5768c5d4f0d026413d730eedbb21b3a11063135
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:33:41 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -2,6 +2,33 @@
 #include "typewise-alert.h"
 #include "message_logger.h"
 
+
+void sendHighTempAlert(const char* recepient)
+{
+    print("To: %s\n", recepient);
+    print("Hi, the temperature is too high\n");
+}
+
+void sendLowTempAlert(const char* recepient)
+{
+    print("To: %s\n", recepient);
+    print("Hi, the temperature is too low\n");
+}
+
+void onNormalTemp(const char* recepient)
+{
+   return;
+}
+
+void sendToEmail(BreachType breachType)
+{
+  const char* recepient = "a.b@c.com";
+
+  void (*messageType[])(const char* recepient) = {onNormalTemp,sendLowTempAlert,sendHighTempAlert};
+
+  messageType[breachType](recepient);
+
+}
 void sendToController(BreachType breachType)
 {
   const unsigned short header = 0xfeed;
