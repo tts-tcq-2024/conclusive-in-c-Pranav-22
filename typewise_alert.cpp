ed0bc843cd442ba7e6d8e18fe984961014191f66
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 11:58:53 +0530

    Update and rename typewise-alert.cpp to typewise_alert.cpp

---
+++ typewise_alert.cpp
@@ -0,0 +1,15 @@
+#include "common_util.h"
+#inlcude "typewise-alert.h"
+#include "send_to_controller.h"
+#include "send_to_email.h"
+
+
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC)
+{
+  BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+
+  /*Switch case is reduced by replacing with function pointers*/
+  void (*sendToTarget[])(BreachType breachType) = {sendToController,sendToEmail};
+
+  sendToTarget[alertTarget](breachType);
+}
