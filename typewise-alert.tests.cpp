22b0d4af5933f7b497ffbe775a24763b6d9ea98a
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 15:24:17 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,8 +2,12 @@
 #include "common_util.h"
 #include "temperature_monitor.h"
 #include "typewise-alert.h"
+extern char testbuffer[10][1024];
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
-
-
+    BatteryCharacter batteryChar;
+    batteryChar.coolingType = HI_ACTIVE_COOLING;
+    double temperatureInC = 50.0;
+    checkAndAlert(TO_CONTROLLER, batteryChar, temperatureInC);
+    ASSERT_EQ("feed : 2",testbuffer[1]);
 }
