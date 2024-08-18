694cc8f639f74d5170a76fbed88eec2733647171
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 11:56:05 +0530

    Create common_util.h

---
+++ common_util.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_UTIL_H
+#define COMMON_UTIL_H
+
+#include <stdio.h>
+#include<string.h>
+#include <stdarg.h>
+#include <assert.h>
+
+typedef enum
+{
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+
+#endif
