39e5b92d4152ba27f0f556ca675532646c6701bb
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:32:29 +0530

    Update send_to_controller.c

--- send_to_controller.c
+++ send_to_controller.c
@@ -1,8 +1,1 @@
 #include "common_util.h"
-#include "message_logger.h"
-
-void sendToController(BreachType breachType)
-{
-  const unsigned short header = 0xfeed;
-  print("%x : %x\n", header, breachType);
-}
