34f1f5e66af427ea5a2e1809c0b584738d998db7
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:33:02 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,5 +1,12 @@
 #include "common_util.h"
 #include "typewise-alert.h"
+#include "message_logger.h"
+
+void sendToController(BreachType breachType)
+{
+  const unsigned short header = 0xfeed;
+  print("%x : %x\n", header, breachType);
+}
 
 
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC)
