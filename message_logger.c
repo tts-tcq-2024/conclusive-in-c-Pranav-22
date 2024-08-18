aea3eadb5d85bf4b0af5d88731f0c6cd0febd159
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-18 13:35:14 +0530

    Update message_logger.c

--- message_logger.c
+++ message_logger.c
@@ -1,20 +1,3 @@
 #include "common_util.h"
 #include "message_logger.h"
 
-char testbuffer[10][1024] = {0};
-int printcount = 0;
-
-void print(const char *format, ...)
-{
-    static int count = 0;
-    va_list args;
-    va_start(args, format);
-    vsprintf(testbuffer[count], format, args);
-    va_end(args);
-    printcount++;
-    count++;
-    if(count == 10)
-    {
-        count = 0;
-    }
-}
